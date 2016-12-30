ce7da9ff5704e94f1afac6123fa0bb3ef8ac2b88
Matt Taylor <mattbtay@gmail.com>
2016-12-30 11:19:42 -0600

    added some basic logic to display status

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
-//batStatus();
+batStatus();
+
+const cs = document.getElementById("charge-status");
 
 function batStatus(){
 
 	navigator.getBattery().then(function(battery){
 		
-		var status = document.getElementById("status-info");
+		const status = document.getElementById("status-info");
+		let batLevel = battery.level * 100,
+			batPercent = battery.level * 281;
 
-		status.innerHTML = `
-			${ battery.level * 100 }%
+
+		/*status.innerHTML = `
+			${ batLevel }%
 			${ battery.charging }
 			${ battery.chargingTime }
 			${ ( battery.dischargingTime / 60 ) / 60 } hours
-			`;
+			`;*/
+
+		
+
+		// full charge width = 281
+		cs.setAttribute('data-full', batPercent );
+		console.log( `${ batPercent } / ${ batLevel }` );
+		
+		setTimeout(function(){
+			cs.setAttribute('width', batPercent);
+		}, 1000);
+
+		if (battery.charging === true) {
+			document.getElementById('charge-path').classList.remove('hidden');
+		}
+
 
 	});
 }
+
